eaa325537ad5445d41262f63cbc985e4e68d3b68
mikaylaaoakes <163308644+mikaylaaoakes@users.noreply.github.com>
2024-06-10 13:48:08 +0200

    Declared variables for Detail Screen

--- app/src/main/java/com/example/myweatherapplication/DetailActivity.kt
+++ app/src/main/java/com/example/myweatherapplication/DetailActivity.kt
@@ -7,5 +7,17 @@ class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        val dates = intent.getStringArrayListExtra("dates")?:arrayListOf()
+        val days = intent.getStringArrayListExtra("days")?:arrayListOf()
+        val sun = intent.getStringArrayListExtra("sun")?:arrayListOf()
+        val minimumTemps = intent.getIntegerArrayListExtra("minimumTemps")?:arrayListOf()
+        val maximumTemps = intent.getIntegerArrayListExtra("maximumTemps")?:arrayListOf()
+        val weatherConditions = intent.getStringArrayListExtra("weatherConditions")?:arrayListOf()
+
+        val viewTextView = findViewById<TextView>(R.id.viewTextView)
+        val averageTempTextView = findViewById<TextView>(R.id.averageTempTextView)
+        val returnButton = findViewById<Button>(R.id.returnButton)
+
     }
 }
--- app/src/main/java/com/example/myweatherapplication/MainActivity.kt
+++ app/src/main/java/com/example/myweatherapplication/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.myweatherapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import android.widget.Intent
 import androidx.lifecycle.ReportFragment.Companion.reportFragment
 
 class MainActivity : AppCompatActivity() {
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         val addButton = findViewById<Button>(R.id.addButton)
         val viewButton = findViewById<Button>(R.id.viewButton)
 
-        addButton.setOnClickListener{
+        addButton.setOnClickListener {
             val day = dayEditText.text.toString()
             val date = dateEditText.text.toString()
             val sunny = sunnyEditText.text.toString()
@@ -41,7 +41,7 @@ class MainActivity : AppCompatActivity() {
             val weatherCondition = weatherEditText.text.toString()
 
             //adding the user input stored in the variables into the arrays
-            if(date.isNotEmpty() && sunny.isNotEmpty() && day.isNotEmpty() && minimumTemp !=null && maximumTemp !=null && weatherCondition.isNotEmpty()) {
+            if (date.isNotEmpty() && sunny.isNotEmpty() && day.isNotEmpty() && minimumTemp != null && maximumTemp != null && weatherCondition.isNotEmpty()) {
                 days.add(day)
                 dates.add(date)
                 sun.add(sunny)
@@ -49,29 +49,52 @@ class MainActivity : AppCompatActivity() {
                 maximumTemps.add(maximumTemp)
                 weatherConditions.add(weatherCondition)
                 Toast.makeText(this, "Input Added", Toast.LENGTH_SHORT).show()
-                clearFields(dayEditText, minTempEditText, maxTempEditText, weatherEditText)
+                clearFields(
+                    dayEditText,
+                    dateEditText,
+                    sunnyEditText,
+                    minTempEditText,
+                    maxTempEditText,
+                    weatherEditText
+                )
             } else {
-                Toast.makeText(this, "Please complete all the Input Fields", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Please complete all the Input Fields", Toast.LENGTH_SHORT)
+                    .show()
             }
+        }
 
             //code to clear array variables when delete button is clicked
             deleteButton.setOnClickListener{
                 days.clear()
+                dates.clear()
+                sun.clear()
                 minimumTemps.clear()
                 maximumTemps.clear()
                 weatherConditions.clear()
                 Toast.makeText(this, "Input Deleted", Toast.LENGTH_SHORT).show()
+            }
 
+            viewButton.setOnClickListener{
+                val intent = Intent(this, DetailActivity::class.java)
+                intent.putStringArrayListExtra("dates", dates)
+                intent.putStringArrayListExtra("days", days)
+                intent.putStringArrayListExtra("sun", sun)
+                intent.putIntegerArrayListExtra("minimumTemps", minimumTemps)
+                intent.putIntegerArrayListExtra("maximumTemps", maximumTemps)
+                intent.putStringArrayListExtra("weatherConditions", weatherConditions)
+
+                startActivity(intent)
+
+            }
 
 
             }
-    }
-}
+
+        }
+
 
     private fun clearFields(vararg fields:EditText) {
         for (field in fields) {
-            field.text.clear()
+         field.text.clear()
         }
-    }
-
     }
